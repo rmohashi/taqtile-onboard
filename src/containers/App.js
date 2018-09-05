b65dda937d46175acd8b6d3438ece528a5c2687e
Rodrigo Masaru Ohashi <rodrigo.ohashi@taqtile.com>
2018-09-05 14:58:33 -0300

    Add route for Home

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import Login from './Login'
+  Route
+} from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: localStorage.getItem('username'),
+    }
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
-          <Route path="/" component={Login} />
+          <Route
+            exact path="/"
+            component={Home}
+          />
+          <Route
+            exact path="/login"
+            component={Login}
+          />
         </div>
       </Router>
     );
